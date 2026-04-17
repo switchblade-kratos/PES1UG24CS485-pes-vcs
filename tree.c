cde5908d2b3c86d44fa5b04980e220a4f9903b81
switchblade-kratos <sushanthlego@gmail.com>
2026-04-17 10:16:22 +0000

    fixed an error in tree.c

--- tree.c
+++ tree.c
@@ -9,7 +9,7 @@
 // Example single entry (conceptual):
 //   "100644 hello.txt\0" followed by 32 raw bytes of SHA-256
 
-
+#include "index.h"
 #include "tree.h"
 #include <stdio.h>
 #include <stdlib.h>
